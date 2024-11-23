71535ca77386cfd82a006716423f5ad47cdf7b59
Michele Vannucci <michele.vannucci20@gmail.com>
2024-11-23 13:31:33 +0100

    Added dynamic p_matrix

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -13,7 +13,7 @@ def expected_costs():
     costs = np.zeros((MAX_ITEMS, MAX_ITEMS))
     for i in range(0, MAX_ITEMS):
         for j in range(0, MAX_ITEMS):
-            state = np.array([i, j]) + 1
+            state = np.array([i, j]) + 1 
             costs[i, j] = H @ state + 5 * (1 if np.any(state == 1) else 0)
     return costs.ravel()
 
@@ -23,8 +23,17 @@ def compute_prob_fixed(x1, x2, y1, y2):
     cond2 = x2 - 1 + (4 if x2 == 1 else 0) <= y2 <= x2 + (4 if x2 == 1 else 0)
     return 1 / 4 if cond1 and cond2 else 0
 
+def compute_prob(x1, x2, y1, y2, t_y1, t_y2):
+    """
+    Compute the probability of transitioning from state (x1, x2) to state (y1, y2) given the sales topping policy t_y1 and t_y2
+    """
+    cond1 = x1 - 1 + (t_y1 if x1 == 1 else 0) <= y1 <= x1 + (t_y2 if x1 == 1 else 0)
+    cond2 = x2 - 1 + (t_y1 if x2 == 1 else 0) <= y2 <= x2 + (t_y2 if x2 == 1 else 0)
+    
 
-def prob_matrix():
+
+
+def prob_matrix_fixed():
     p = np.zeros((MAX_ITEMS**2, MAX_ITEMS**2))
     for i in range(MAX_ITEMS**2):
         for j in range(MAX_ITEMS**2):
@@ -34,6 +43,19 @@ def prob_matrix():
             p[i, j] = compute_prob_fixed(x1, x2, y1, y2)
     assert (np.sum(p, axis=1) == 1).all()
     return p
+
+# the same as above but with with the funcion to compute the probability for the single element as an argument
+def prob_matrix( prob_func, *args):
+    p = np.zeros((MAX_ITEMS**2, MAX_ITEMS**2))
+    for i in range(MAX_ITEMS**2):
+        for j in range(MAX_ITEMS**2):
+            x1, x2 = divmod(i, MAX_ITEMS)
+            y1, y2 = divmod(j, MAX_ITEMS)
+            x1, x2, y1, y2 = (v + 1 for v in (x1, x2, y1, y2))  # Indices start at 0
+            p[i, j] = prob_func(x1, x2, y1, y2, *args)
+    assert (np.sum(p, axis=1) == 1).all()
+    return p
+
 
 
 def question_c(n=10_000):
@@ -66,21 +88,28 @@ def question_c(n=10_000):
 
 def question_d(n=10_000):
     pi = np.eye(MAX_ITEMS**2)[-1]
-    p = prob_matrix()
+    p = prob_matrix_fixed()
     for _ in range(n):
         pi = pi @ p
     return pi @ expected_costs(), pi
 
+def geneat
 
 def question_e(n=10_000, eps=10e-5):
     v = np.zeros((MAX_ITEMS**2))
     r = expected_costs()
-    p = prob_matrix()
+    p = prob_matrix_fixed()
     for _ in range(n):
         v = r + p @ v
     phi_star = r + p @ v - v
     assert np.ptp(phi_star) < eps
     return (v - v.min(), phi_star[0])
+
+# Question f: Define the Bellman Equation and solve it using value iteration (minimizing the long-run average costs). Find the optimal policy and report the corresponding long-run average costs.
+
+def 
+
+
 
 
 def main():
