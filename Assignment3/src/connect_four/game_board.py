5f7674bf6dd0cc8ff11d86c9fa543a0a58219e08
Michele Vannucci <michele.vannucci20@gmail.com>
2024-12-05 15:58:32 +0100

    Implemented gameboard

--- Assignment3/src/connect_four/game_board.py
+++ Assignment3/src/connect_four/game_board.py
@@ -10,44 +10,92 @@ class PlayerType(IntEnum):
     OPPONENT = 2
 
 
+# Import the convolution function
+from scipy.signal import convolve2d
+
 class GameBoard:
     grid: np.ndarray
 
-    # TODO
     @property
-    def is_finished(self) -> bool: ...  # Check if the matrix `self.grid` is full or not
+    def is_finished(self) -> bool: 
+        
+        return 0 not in self.grid  # Check if the matrix `self.grid` is full or not
 
-    # TODO
     @property
     def available_actions(self) -> Set[int]:
-        return ...  # Just a set with available actions, like {5, 7} or {5,}
 
-    # TODO
-    def __init__(self, nrows: int, ncols: int) -> None:
-        self.grid = ...  # Init with the grid shown on the assignment pdf
+        free_columns = set(np.where(self.grid == 0)[1])
+        return {int(i) for i in free_columns} # not needed if np.int64 type is okay
+  
+    def __init__(self, nrows: int, ncols: int, inital_state = True) -> None:
 
+        if inital_state:
+            # Init with the grid shown on the assignment pdf
+            assert nrows == 6 and ncols == 7
+            self.grid = np.array(
+                [
+                    [2, 2, 2, 1, 0, 1, 0],
+                    [2, 1, 1, 1, 0, 2, 0],
+                    [1, 2, 2, 2, 0, 1, 0],
+                    [2, 1, 1, 1, 0, 2, 0],
+                    [1, 1, 1, 2, 0, 2, 0],
+                    [2, 2, 1, 2, 0, 1, 0],
+                ]
+            )
+        else:
+            self.grid = np.zeros((nrows, ncols))
     def snapshot(self) -> np.ndarray:
         return self.grid.copy()
 
-    # TODO
-    def game_result(self) -> int:
+    # TODO test this method more
+    def game_result(self) -> Optional[int]: 
         if not self.is_finished:
-            return 0  # No matter if game is not done yet
-        return ...  # [1, 0, -1] depending on result
+            return None  # No matter if game is not done yet
+        
+        # check for horizontal win
+        # Define the kernel for convolution-like checks
+        kernels = {
+            "horizontal": np.array([[1, 1, 1, 1]]),  # Horizontal pattern
+            "vertical": np.array([[1], [1], [1], [1]]),  # Vertical pattern
+            "diagonal_down": np.eye(4, dtype=int),  # Diagonal down (\)
+            "diagonal_up": np.fliplr(np.eye(4, dtype=int)),  # Diagonal up (/)
+        }
+        winner = None
+        for player in [1, 2]:
+            # Create a binary grid where the player's pieces are 1 and others are 0
+            player_grid = (self.grid == player).astype(int)
+
+            for kernel in kernels.values():
+                # Perform convolution between the player's grid and the kernel
+                conv_result = convolve2d(player_grid, kernel, mode='valid')
+
+                # If any position in conv_result equals 4, the player has won
+                if np.any(conv_result == 4):
+                    if winner == 1:
+                        raise ValueError("Both players can't win at the same time")
+                    winner = player
+                    
+
+        # If no winner and the board is full, it's a draw
+        return winner
 
     # TODO
     def step(self, action: int, player: int) -> bool:
         action = self.validate_action(action)
         if action is None:
             return self.is_finished  # If action is non valid, just leave it like it is
-        ...  # update the `self.grid` according to `action` and `player` type
+        
+        row = np.max(np.where(self.grid[:, action] == 0))
+
+        self.grid[row, action] = player
+        
         return self.is_finished
 
-    # TODO
     def validate_action(self, action: int) -> Optional[int]:
-        if ...:  # Check whether `action` column of our `self.grid` has spots left
-            return None  # Don't panic if is invalid, let `step` handle a `None`
-        return action
+        if 0 in self.grid[:, action]:
+            return action
+        else:
+            return None
 
     def play(self, action: int, state: Optional[np.ndarray] = None) -> Tuple[int, bool]:
         # Choose to either start from state or just keep going with current stored state
